1306db43b410fcdbb277973994e6879a5827ae48
Martin Rancourt <martin@martinrancourt.com>
2025-05-11 20:53:50 -0400

    Updated index.ts

--- supabase/functions/process-document/index.ts
+++ supabase/functions/process-document/index.ts
@@ -17,6 +17,7 @@ serve(async (req) => {
   }
 
   try {
+    console.log('SUPABASE_SERVICE_ROLE_KEY', Deno.env.get('SUPABASE_SERVICE_ROLE_KEY'));
     const supabaseClient = createClient(
       Deno.env.get('SUPABASE_URL') ?? '',
       Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? '',
